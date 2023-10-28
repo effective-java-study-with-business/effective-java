911cd99954f196464b92cdf3dd7c75562a84e79f
Youngseok Kim <70935170+dudtjr0831@users.noreply.github.com>
2023-10-28 22:33:27 +0900

    Create AutoBoxing.java

---
+++ src/item6/AutoBoxing.java
@@ -0,0 +1,9 @@
+public class Main {
+     public static void main(String[] args) {
+        Long sum = 0L;
+        for (long i = 0 ; i <= Integer.MAX_VALUE ; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
